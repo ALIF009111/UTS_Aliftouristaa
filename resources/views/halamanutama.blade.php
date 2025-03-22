b7aa8c2027b31267576bf87a4498dad5186455b2
Aliftourista <aliftourista@gmail.com>
2025-03-22 12:14:27 +0700

    updatehalamanutama

--- resources/views/halamanutama.blade.php
+++ resources/views/halamanutama.blade.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Index - Selecao Bootstrap Template</title>
+  <title>Halaman Utama</title>
   <meta name="description" content="">
   <meta name="keywords" content="">
 
@@ -27,132 +27,78 @@
   <link href="{{asset('template/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
 
   <!-- Main CSS File -->
-  <link href="{{asset('template/css/main.css')}}" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Selecao
-  * Template URL: https://bootstrapmade.com/selecao-bootstrap-template/
-  * Updated: Aug 07 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
+  <link href="{{asset('template/css/main.css')}}" rel="stylesheet">q
 </head>
 
 <body class="index-page">
 
-  <header id="header" class="header d-flex align-items-center fixed-top">
-    <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+    <style>
+        .navmenu ul li a {
+            color: black !important; /* Ubah warna teks navbar menjadi hitam */
+            font-weight: 600;
+            transition: color 0.3s ease-in-out;
+        }
 
-      <a href="index.html" class="logo d-flex align-items-center">
-        <!-- Uncomment the line below if you also wish to use an image logo -->
-        <!-- <img src="assets/img/logo.png" alt=""> -->
-        <img src="{{asset('template/img/iconbsi.png')}}" alt="">
-      </a>
+        .navmenu ul li a:hover {
+            color: #555 !important; /* Warna lebih gelap saat hover */
+        }
+    </style>
 
-      <nav id="navmenu" class="navmenu">
-        <ul>
-          <li><a href="#hero" class="active">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#team">Team</a></li>
-          <li><a href="blog.html">Blog</a></li>
-          <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-            <ul>
-              <li><a href="#">Dropdown 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+    <header id="header" class="header d-flex align-items-center fixed-top bg-white shadow">
+        <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+            <a href="index.html" class="logo d-flex align-items-center">
+                <img src="{{asset('template/img/iconbsi.png')}}" alt="">
+            </a>
+            <nav id="navmenu" class="navmenu">
                 <ul>
-                  <li><a href="#">Deep Dropdown 1</a></li>
-                  <li><a href="#">Deep Dropdown 2</a></li>
-                  <li><a href="#">Deep Dropdown 3</a></li>
-                  <li><a href="#">Deep Dropdown 4</a></li>
-                  <li><a href="#">Deep Dropdown 5</a></li>
+                    <li><a href="#hero" class="active">Produk dan Layanan</a></li>
+                    <li><a href="#about">Prioritas</a></li>
+                    <li><a href="#services">Berita & Pembaruan</a></li>
+                    <li><a href="#portfolio">Informasi Perusahaan</a></li>
+                    <li><a href="#team">Hubungan Investor</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="#contact">Contact</a></li>
                 </ul>
-              </li>
-              <li><a href="#">Dropdown 2</a></li>
-              <li><a href="#">Dropdown 3</a></li>
-              <li><a href="#">Dropdown 4</a></li>
-            </ul>
-          </li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-      </nav>
-
-    </div>
-  </header>
-
-  <main class="main">
-
-    <!-- Hero Section -->
-    <section id="hero" style="background-image: url('template/img/bgbsi.jpg'); background-size: cover; background-position: center; background-attachment: fixed;">
-
-        <div id="hero-carousel" data-bs-interval="5000" class="container carousel carousel-fade" data-bs-ride="carousel">
-
-          <!-- Slide 1 -->
-          <div class="carousel-item active">
-            <div class="carousel-container">
-              <h2 class="animate__animated animate__fadeInDown">WEBSITE ALIF JOMOK</h2>
-              <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-              <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
-            </div>
-          </div>
-
-          <!-- Slide 2 -->
-          <div class="carousel-item">
-            <div class="carousel-container">
-              <h2 class="animate__animated animate__fadeInDown">Lorem Ipsum Dolor</h2>
-              <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-              <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
-            </div>
-          </div>
-
-          <!-- Slide 3 -->
-          <div class="carousel-item">
-            <div class="carousel-container">
-              <h2 class="animate__animated animate__fadeInDown">Sequi ea ut et est quaerat</h2>
-              <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-              <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
-            </div>
-          </div>
-
-          <!-- Carousel Controls -->
-          <a class="carousel-control-prev" href="#hero-carousel" role="button" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
-          </a>
-          <a class="carousel-control-next" href="#hero-carousel" role="button" data-bs-slide="next">
-            <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
-          </a>
-
+                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+            </nav>
         </div>
+    </header>
 
-        <!-- SVG Waves -->
-        <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none">
-          <defs>
-            <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"></path>
-          </defs>
-          <g class="wave1">
-            <use xlink:href="#wave-path" x="50" y="3"></use>
-          </g>
-          <g class="wave2">
-            <use xlink:href="#wave-path" x="50" y="0"></use>
-          </g>
-          <g class="wave3">
-            <use xlink:href="#wave-path" x="50" y="9"></use>
-          </g>
-        </svg>
+    <br>
+    <br>
+    <br>
 
-      </section>
+    <main class="main">
+        <section id="hero" class="d-flex align-items-center"
+            style="background: url('template/img/bgbsi.jpg') center/cover no-repeat;
+                   height: 80vh; padding-top: 100px;">
+            <div id="hero-carousel" class="container carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
+
+                        <a class="carousel-control-prev rounded-circle d-flex align-items-center justify-content-center"
+        href="#hero-carousel" role="button" data-bs-slide="prev" style="width: 50px; height: 50px; background-color: rgba(0, 0, 0, 0.5);">
+            <span class="carousel-control-prev-icon bi bi-chevron-left text-white"></span>
+        </a>
+
+        <a class="carousel-control-next rounded-circle d-flex align-items-center justify-content-center"
+        href="#hero-carousel" role="button" data-bs-slide="next" style="width: 50px; height: 50px; background-color: rgba(0, 0, 0, 0.5);">
+            <span class="carousel-control-next-icon bi bi-chevron-right text-white"></span>
+        </a>
+
+            </div>
+        </section>
+    </main>
+
 
 
     <!-- About Section -->
     <section id="about" class="about section">
 
       <!-- Section Title -->
-      <div class="container section-title" data-aos="fade-up">
-        <h2>About</h2>
-        <p>Who we are</p>
-      </div><!-- End Section Title -->
+      <div class="container section-title text-center" data-aos="fade-up">
+        <h3>Assalamu'alaikum Warahmatullahi Wabarakatuh</h3>
+        <p>Jadikan pengalaman Kamu lebih baik hari ini</p>
+    </div>
+    <!-- End Section Title -->
 
       <div class="container">
 
@@ -180,31 +126,13 @@
       </div>
 
     </section><!-- /About Section -->
-    <footer id="footer" class="footer dark-background">
-    <div class="container">
-      <h3 class="sitename">Selecao</h3>
-      <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
-      <div class="social-links d-flex justify-content-center">
-        <a href=""><i class="bi bi-twitter-x"></i></a>
-        <a href=""><i class="bi bi-facebook"></i></a>
-        <a href=""><i class="bi bi-instagram"></i></a>
-        <a href=""><i class="bi bi-skype"></i></a>
-        <a href=""><i class="bi bi-linkedin"></i></a>
-      </div>
-      <div class="container">
+    <footer id="footer" class="footer bg-black text-white py-4 text-center">
+
+
         <div class="copyright">
-          <span>Copyright</span> <strong class="px-1 sitename">Selecao</strong> <span>All Rights Reserved</span>
+          <span>Copyright</span> <strong class="px-1 sitename">Aliftourista</strong> <span>All Rights Reserved</span>
         </div>
-        <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you've purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-        </div>
-      </div>
-    </div>
-  </footer>
+    </footer>
 
   <!-- Scroll Top -->
   <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
